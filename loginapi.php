62be035a33e1b63cc1cc71985290f21121a6e7a0
karndevza <karnkung30@gmail.com>
2020-11-07 15:17:42 +0700

    Create loginapi.php

---
+++ loginapi.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="generator" content="">
+    <title>login</title>
+
+    <link rel="canonical" href="">
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
+    <meta name="google-signin-client_id" content="1047015305228-usug13ac84ouvk3n1c8tbte6mofekh3c.apps.googleusercontent.com">
+
+</head>
+<body>
+<div class="g-signin2" data-onsuccess="onSignIn"></div>
+
+
+<script>
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+</script>
+</body>
+
+</html>
